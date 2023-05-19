a1892591a5a4ed5074226a5c8e697a6d306a27b6
mayuri2307 <31414105+mayuri2307@users.noreply.github.com>
2023-05-19 17:20:11 +0530

    Delete frontend-chat-design directory

--- frontend-chat-design/src/App.js
+++
@@ -1,9 +0,0 @@
-import ShopSenseMain from "./pages/ShopSenseMain";
-
-function App() {
-
-  return (
-      <ShopSenseMain />
-  );
-}
-export default App;
--- frontend-chat-design/src/utils/Routes.js
+++
@@ -1,3 +0,0 @@
-export const node_base_url = "http://localhost:5000"
-
-export const python_base_url = "http://localhost:5001"
