8e60ed847d97f1c9ae0e787a75eaa7ff94ee7ba7
Mayuri Gupta <mg153@snu.edu.in>
2023-06-02 14:15:21 +0530

    if applied, this commit prepares for netlify

--- python-backend/app.py
+++ python-backend/app.py
@@ -11,5 +11,7 @@ app.register_blueprint(llm_openai, url_prefix="/predict")
 def index():
     return 'Web App with Python Flask!'
 
-app.run(host='127.0.0.1', port=5001)
+if __name__ == "__main__":
+    app.run(debug=True)
+    #app.run(host='127.0.0.1', port=5001)
 
